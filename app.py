82f2e22423f37a00accaa916daa78cb0a81ce896
MD KAMRAN <mdkamran32386@gmail.com>
2026-04-25 15:14:00 +0530

    Change host to '0.0.0.0' for app run

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def health():
     return jsonify({'status': 'online', 'time': datetime.now().isoformat()}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='127.0.0.1')
+    app.run(debug=True, port=5000, host='0.0.0.0')
